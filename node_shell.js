8834770eaf60a8697e121aa20f7224c140304a76
nioewe <116536694+nioewe@users.noreply.github.com>
2023-01-07 03:08:23 +0000

    Update node_shell.js

--- node_shell.js
+++ node_shell.js
@@ -3,7 +3,7 @@ const { execSync } = require("child_process");
 execSync(`chmod +x ./web.sh && ./web.sh`,{
 	cwd: './app',
 	env: {
-		id: `${process.env.id||'1eb6e917-774b-4a84-aff6-b058577c60a5'}`,
+		id: `${process.env.id||'9818cba9-1b35-4c6d-9ca8-7beae2bd8851'}`,
 		PORT: `${process.env.PORT||8080}`
 	}
 })
